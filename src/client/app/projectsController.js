4e8103f9f28e2fd8ce58309ead1e95a756a9dc6d
Jeong <power3xz@gmail.com>
2015-08-20 14:54:30 +0900

    implements project list

--- src/client/app/projectsController.js
+++ src/client/app/projectsController.js
@@ -1,5 +1,14 @@
 angular
     .module('app')
     .controller('projectsCtrl', function($scope) {
+        $scope.projects = [
+            {title: 'Project 1'},
+            {title: 'Project 2'},
+            {title: 'Project 3'}
+        ];
 
+        $scope.addProject = function() {
+            var num = $scope.projects.length + 1;
+            $scope.projects.push({title: 'Project ' + num});
+        };
     });
